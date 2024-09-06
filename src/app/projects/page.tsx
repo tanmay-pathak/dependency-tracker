ac2c8f92517c5737e86a7bcbc11f7b61525ef56a
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-06 15:55:00 -0600

    feat: landing page and typography plugin

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -21,7 +21,7 @@ export default async function Page() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 bg-background">
             {uniqueProjects.map((project) => (
-                <Link href={`/visualizer/${project}`} key={project} className="block">
+                <Link href={`/projects/${project}`} key={project} className="block">
                     <Card className="hover:bg-accent transition-colors">
                         <CardHeader>
                             <CardTitle className="text-primary">{project}</CardTitle>
--- tailwind.config.js
+++ tailwind.config.js
@@ -83,5 +83,8 @@ module.exports = {
   		}
   	}
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-animate')
+  ]
 }
