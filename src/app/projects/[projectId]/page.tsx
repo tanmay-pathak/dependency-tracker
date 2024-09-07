bb923bceec996c52a19ed79179dcddb31ad365cd
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-06 23:15:57 -0600

    fix: hover effect disable

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -25,7 +25,7 @@ export default async function Page({
 
   return (
     <div className="container mx-auto p-2">
-      <Card>
+      <Card hover={false}>
         <CardHeader className="flex flex-row items-center justify-between">
           <CardTitle>Project Dependencies</CardTitle>
           <Link href={`/projects/${projectId}/dependencies`}>
