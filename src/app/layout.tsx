38a8e1dcb614f80eff69504a75c703759f6f73e3
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-07 15:35:59 -0600

    chore: minor updates to copy and page title (#14)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Dependency Tracker',
+  description: 'Track your dependencies across all of your projects',
 }
 
 export default function RootLayout({
