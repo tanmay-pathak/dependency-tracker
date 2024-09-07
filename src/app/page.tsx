0f6441f53743239154e8de1b3bd1d208865015db
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-07 15:35:10 -0600

    chore: minor updates to copy and page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,10 @@ export default async function Index() {
     <div className="flex w-full flex-1 flex-col items-center gap-20">
       <Header />
       <main className="prose prose-lg text-center dark:prose-invert">
-        <h1>Welcome to zu dependency tracker</h1>
+        <h1>Welcome to dependency tracker</h1>
+        <h4>
+          Track your dependencies across all of your projects
+        </h4>
         <div className="mx-auto flex w-fit flex-col gap-4">
           <Link href="/projects">
             <Button>View Projects</Button>
