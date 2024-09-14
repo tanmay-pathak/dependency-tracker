cc36b96097ce8c03447ed4299493cfa2a6be5286
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-14 16:38:46 -0600

    fix: project layout on wide screens

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -18,7 +18,7 @@ export default async function Projects() {
   const uniqueProjects = Array.from(new Set(dependencies?.map((dep) => dep.id)))
 
   return (
-    <div className="p-4">
+    <div className="container mx-auto p-4">
       <ProjectList projects={uniqueProjects} />
     </div>
   )
