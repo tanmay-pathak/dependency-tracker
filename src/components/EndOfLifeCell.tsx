eec4717f7fa07dd78ee073c36539804d033bb399
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-19 17:18:09 -0600

    refactor: extracted query into a hook

--- src/components/EndOfLifeCell.tsx
+++ src/components/EndOfLifeCell.tsx
@@ -3,65 +3,24 @@
 import { Loader2 } from 'lucide-react'
 import { Badge } from '@/components/ui/badge'
 import { dependencyBySearch } from '@/constants/dependency-mappings'
-import { useQuery } from '@tanstack/react-query'
 import { InfoTooltip } from '@/components/InfoTooltip'
+import useFetchVersionData from '@/hooks/useFetchVersionData'
 
 interface EndOfLifeCellProps {
   searchKey: string
   version: string
 }
 
-export async function fetchVersionData(tech: string, version: string) {
-  let actualTech = tech
-  if (tech === 'db') {
-    if (version.toLowerCase().includes('mariadb')) {
-      actualTech = 'mariadb'
-    } else {
-      actualTech = 'mysql'
-    }
-  }
-
-  async function fetchDataForSpecificVersion(versionToFetch: string) {
-    const url = `https://endoflife.date/api/${actualTech}/${versionToFetch}.json`
-    const response = await fetch(url)
-
-    if (response.status === 404) return null
-    if (!response.ok) throw new Error('Failed to fetch EOL data')
-
-    return (await response.json()) as Record<string, string>
-  }
-
-  let versionToTry = version
-  let data = null
-
-  while (versionToTry) {
-    data = await fetchDataForSpecificVersion(versionToTry)
-    if (data) break
-
-    const versionParts = versionToTry.split('.')
-    versionParts.pop()
-    versionToTry = versionParts.join('.')
-  }
-
-  if (!data) throw new Error('Failed to fetch version data')
-
-  return { Tech: actualTech, Showing_Info_For_Version: versionToTry, ...data }
-}
-
 export function EndOfLifeCell({ searchKey, version }: EndOfLifeCellProps) {
   const dependency = dependencyBySearch[searchKey.toLowerCase()]
 
-  const { data, isLoading } = useQuery({
-    queryKey: ['eolData', dependency?.tech, version],
-    queryFn: () => fetchVersionData(dependency?.tech || '', version),
-    enabled: !!dependency?.tech,
-    staleTime: 1000 * 60 * 60 * 4, // 4 hours
-    retry: 3,
-  })
+  const { data, isLoading } = useFetchVersionData(
+    dependency?.tech || '',
+    version,
+  )
 
   if (isLoading) return <Loader2 className="h-4 w-4 animate-spin" />
 
-  // @ts-expect-error - No proper type hints for this API exists.
   const eolValue = data?.eol
   const eolDisplay = getEolDisplay(eolValue)
   const isPastEol = checkIsPastEol(eolDisplay)
