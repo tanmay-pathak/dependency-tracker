444d38ff18244400af1c6f7052b7f3f4a13b7c2f
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-19 21:09:45 -0600

    feat: versions page side by side

---
+++ src/app/versions/[search]/project/page.tsx
@@ -0,0 +1,43 @@
+import { createServerClient } from '@/utils/supabase'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import { Dependency } from '@/app/versions/[search]/columns'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft } from 'lucide-react'
+import { ProjectVersionsTable } from '@/components/project-versions-table'
+
+export default async function VersionsPage({
+  params: { search },
+}: {
+  params: { search: string }
+}) {
+  const cookieStore = cookies()
+  const supabase = createServerClient(cookieStore)
+
+  const { data, error } = await supabase
+    .from('versions')
+    .select('*')
+    .ilike('key', `%${search}%`)
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+  const dependencies: Dependency[] = data || []
+
+  return (
+    <div className="container mx-auto p-6">
+      <div className="relative mb-4">
+        <div className="absolute left-0 top-1/2 -translate-y-1/2">
+          <Link href={`/versions`}>
+            <Button variant="outline" size="sm">
+              <ArrowLeft className="mr-2 h-4 w-4" />
+              Versions
+            </Button>
+          </Link>
+        </div>
+        <h2 className="text-center text-2xl font-bold uppercase">{search}</h2>
+      </div>
+      <ProjectVersionsTable data={dependencies} tech={search} />
+    </div>
+  )
+}
