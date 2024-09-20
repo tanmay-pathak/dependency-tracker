63025921620a8ea47c727957457d6c865872bf78
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-20 17:51:04 -0600

    Simple Project View (#40)
    * feat: simplified project tools page
    
    * refactor: simple view
    
    * fix: order
    
    * refactor: path rename
    
    * fix: eslint config

--- src/app/projects/[projectId]/simple-tools/page.tsx
+++ src/app/projects/[projectId]/simple-tools/page.tsx
@@ -3,10 +3,10 @@ import { cookies } from 'next/headers'
 import Link from 'next/link'
 import { Dependency } from '@/constants/types'
 import { Button } from '@/components/ui/button'
-import { ArrowLeft, List } from 'lucide-react'
-import { ProjectToolsTable } from '@/components/project-tools-table'
+import { ArrowLeft } from 'lucide-react'
+import { SimpleProjectToolsTable } from '@/components/simple-project-tools-table'
 
-export default async function ToolsPage({
+export default async function FuturePage({
   params: { projectId },
 }: {
   params: { projectId: string }
@@ -18,34 +18,30 @@ export default async function ToolsPage({
     .from('versions')
     .select('*')
     .eq('id', projectId)
+    .eq('environment', 'DEV')
+    .ilike('key', '%VERSION%')
+    .order('key', { ascending: true })
 
   if (error) {
     return <div>Error: {error.message}</div>
   }
+
   const dependencies: Dependency[] = data || []
 
   return (
     <div className="container mx-auto p-6">
       <div className="relative mb-4">
         <div className="absolute left-0 top-1/2 -translate-y-1/2">
-          <Link href={`/projects`}>
+          <Link href={`/projects/${projectId}/tools`}>
             <Button variant="outline" size="sm">
               <ArrowLeft className="mr-2 h-4 w-4" />
-              Projects
+              Detailed View
             </Button>
           </Link>
         </div>
         <h2 className="text-center text-2xl font-bold">{projectId}</h2>
-        <div className="absolute right-0 top-1/2 -translate-y-1/2">
-          <Link href={`/projects/${projectId}/simple-tools`}>
-            <Button variant="outline" size="sm">
-              <List className="mr-2 h-4 w-4" />
-              Simple View
-            </Button>
-          </Link>
-        </div>
       </div>
-      <ProjectToolsTable data={dependencies} />
+      <SimpleProjectToolsTable data={dependencies} />
     </div>
   )
 }
---
+++ src/components/simple-project-tools-table.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Dependency } from '@/constants/types'
+import { EndOfLifeCell } from './EndOfLifeCell'
+import { dependencyBySearch } from '@/constants/dependency-mappings'
+
+interface ProjectToolsTableProps {
+  data: Dependency[]
+}
+
+export function SimpleProjectToolsTable({ data }: ProjectToolsTableProps) {
+  return (
+    <div className="flex h-full flex-col">
+      <div className="flex-grow overflow-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-1/2">Tool</TableHead>
+              <TableHead className="w-1/2">End of Life</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((item) => (
+              <TableRow key={item.key}>
+                <TableCell>
+                  {dependencyBySearch[item.key.toLowerCase()].name}
+                </TableCell>
+                <TableCell>
+                  <EndOfLifeCell searchKey={item.key} version={item.value} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
