23b6e8688c01a9680c79ea5fcac8c12375a3a068
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-09-21 16:13:05 -0600

    fix: table head

--- src/components/project-versions-table.tsx
+++ src/components/project-versions-table.tsx
@@ -58,7 +58,7 @@ export function ProjectVersionsTable({
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead>Tool</TableHead>
+              <TableHead>Project</TableHead>
               <TableHead>LOCAL</TableHead>
               <TableHead>DEV</TableHead>
               <TableHead>BETA</TableHead>
