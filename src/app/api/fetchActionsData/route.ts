337bf87ab322920ed5193576d3c1b509b62d52e8
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-10-30 11:18:00 -0600

    refactor: server action

---
+++ src/app/api/fetchActionsData/route.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from 'next/server'
+import axios from 'axios'
+
+export type ActionOptions = {
+  repoName: string
+  owner: string
+  filter:
+    | 'completed'
+    | 'action_required'
+    | 'cancelled'
+    | 'failure'
+    | 'neutral'
+    | 'skipped'
+    | 'stale'
+    | 'success'
+    | 'timed_out'
+    | 'in_progress'
+    | 'queued'
+    | 'requested'
+    | 'waiting'
+    | 'pending'
+}
+
+export type ghActions = {
+  total_count: number
+  workflow_runs: any[]
+}
+
+const fetchTechData = async (options: ActionOptions): Promise<ghActions> => {
+  const url = `https://api.github.com/repos/${options.owner}/${options.repoName}/actions/runs?status=${options.filter}`
+  try {
+    const { data } = await axios.get<ghActions>(url, {
+      headers: {
+        Authorization: `${process.env.GH_ACCESS_TOKEN}`,
+      },
+    })
+
+    return data
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 404) {
+      return { total_count: 0, workflow_runs: [] }
+    }
+    throw new Error('Failed to fetch tech data')
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const options: ActionOptions = await req.json()
+  try {
+    const data = await fetchTechData(options)
+    return NextResponse.json(data)
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : 'Unknown error'
+    return NextResponse.json({ message: errorMessage }, { status: 500 })
+  }
+}
--- src/hooks/useFetchActionsData.ts
+++ src/hooks/useFetchActionsData.ts
@@ -26,25 +26,11 @@ export type ghActions = {
   workflow_runs: any[]
 }
 
-export async function fetchTechData(
+const fetchActionsDataFromServer = async (
   options: ActionOptions,
-): Promise<ghActions> {
-  const url = `https://api.github.com/repos/${options.owner}/${options.repoName}/actions/runs?status=${options.filter}`
-  const GH_ACCESS_TOKEN = process.env.NEXT_PUBLIC_GH_ACCESS_TOKEN
-  try {
-    const { data } = await axios.get<ghActions>(url, {
-      headers: {
-        Authorization: `${GH_ACCESS_TOKEN}`,
-      },
-    })
-
-    return data
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response?.status === 404) {
-      return { total_count: 0, workflow_runs: [] }
-    }
-    throw new Error('Failed to fetch tech data')
-  }
+): Promise<ghActions> => {
+  const { data } = await axios.post<ghActions>('/api/fetchActionsData', options)
+  return data
 }
 
 const useFetchActionsData = (
@@ -52,7 +38,7 @@ const useFetchActionsData = (
 ): UseQueryResult<ghActions, Error> =>
   useQuery({
     queryKey: ['actionsData', options],
-    queryFn: () => fetchTechData(options),
+    queryFn: () => fetchActionsDataFromServer(options),
     enabled: options.repoName !== '' && options.owner !== '',
     staleTime: 1000 * 60 * 60 * 24, // 1 day
     refetchInterval: 1000 * 60 * 60 * 24, // 1 day
