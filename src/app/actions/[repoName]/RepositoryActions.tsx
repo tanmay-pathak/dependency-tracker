858b1c3ec64e37293ada3b2f5a6569d7525f6c49
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-10-31 07:33:21 -0600

    feat: skeleton loading

--- src/app/actions/[repoName]/RepositoryActions.tsx
+++ src/app/actions/[repoName]/RepositoryActions.tsx
@@ -1,11 +1,12 @@
 'use client'
 import ActionCard from '@/components/ActionCard'
+import { Skeleton } from '@/components/ui/skeleton'
 import useFetchActionsData from '@/hooks/useFetchActionsData'
 
 type Props = { repoName: string }
 
 const RepositoryActions = ({ repoName }: Props) => {
-  const { data } = useFetchActionsData({
+  const { data, isLoading } = useFetchActionsData({
     repoName: repoName as string,
     owner: process.env.NEXT_PUBLIC_GITHUB_OWNER ?? '',
     filter: 'completed',
@@ -16,19 +17,34 @@ const RepositoryActions = ({ repoName }: Props) => {
       <div className="mb-4">
         <h2 className="text-center text-2xl font-bold uppercase">{repoName}</h2>
       </div>
-      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
-        {data?.workflow_runs.map((action) => (
-          <ActionCard
-            key={action.id}
-            title={action.name}
-            branch={action.head_branch}
-            runStart={action.created_at}
-            status={action.status}
-            conclusion={action.conclusion}
-            link={action.html_url}
-          />
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {Array.from({ length: 9 }).map((_, index) => (
+            <div key={index} className="flex flex-col space-y-2">
+              <Skeleton className="h-[150px] w-[350px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[300px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+          {data?.workflow_runs.map((action) => (
+            <ActionCard
+              key={action.id}
+              title={action.name}
+              branch={action.head_branch}
+              runStart={action.created_at}
+              status={action.status}
+              conclusion={action.conclusion}
+              link={action.html_url}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
--- src/app/actions/page.tsx
+++ src/app/actions/page.tsx
@@ -4,31 +4,44 @@ import useFetchRepos from '@/hooks/useFetchRepos'
 import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import { ExternalLink } from 'lucide-react'
 import Link from 'next/link'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export default function ActionsHomePage() {
-  const { data } = useFetchRepos({
+  const { data, isLoading } = useFetchRepos({
     owner: process.env.NEXT_PUBLIC_GITHUB_OWNER ?? '',
   })
 
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {data?.map((repo) => (
-          <Link
-            href={`/actions/${repo.name}`}
-            key={repo.full_name}
-            className="block"
-          >
-            <Card className="relative h-full">
-              <CardHeader>
-                <CardTitle>
-                  <span className="text-2xl font-bold">{repo.name}</span>
-                </CardTitle>
-              </CardHeader>
-              <ExternalLink className="absolute right-4 top-4 size-4 text-muted-foreground" />
-            </Card>
-          </Link>
-        ))}
+        {isLoading ? (
+          <>
+            {Array.from({ length: 10 }).map((_, index) => (
+              <div className="flex flex-col space-y-3">
+                <Skeleton className="h-[100px] w-[250px] rounded-xl" />
+              </div>
+            ))}
+          </>
+        ) : (
+          <>
+            {data?.map((repo) => (
+              <Link
+                href={`/actions/${repo.name}`}
+                key={repo.full_name}
+                className="block"
+              >
+                <Card className="relative h-full">
+                  <CardHeader>
+                    <CardTitle>
+                      <span className="text-2xl font-bold">{repo.name}</span>
+                    </CardTitle>
+                  </CardHeader>
+                  <ExternalLink className="absolute right-4 top-4 size-4 text-muted-foreground" />
+                </Card>
+              </Link>
+            ))}
+          </>
+        )}
       </div>
     </div>
   )
