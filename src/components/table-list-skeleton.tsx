ad906e05c2520d52ddb8e46db137e209305e65a0
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-11-13 09:36:45 -0600

    refactor: security alerts table for better loading ux

--- src/components/table-list-skeleton.tsx
+++ src/components/table-list-skeleton.tsx
@@ -1,11 +1,11 @@
 import { Skeleton } from './ui/skeleton'
 import {
   Table,
-  TableHeader,
-  TableRow,
-  TableHead,
   TableBody,
   TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from './ui/table'
 
 export const TableSkeleton = () => {
@@ -20,23 +20,29 @@ export const TableSkeleton = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {Array.from({ length: 10 }).map((_, i) => (
-          <TableRow key={i}>
-            <TableCell>
-              <Skeleton className="h-4 w-24" />
-            </TableCell>
-            <TableCell className="w-10">
-              <Skeleton className="h-4 w-10" />
-            </TableCell>
-            <TableCell>
-              <Skeleton className="h-4 w-4" />
-            </TableCell>
-            <TableCell>
-              <Skeleton className="h-4 w-16" />
-            </TableCell>
-          </TableRow>
+        {Array.from({ length: 15 }).map((_, i) => (
+          <TableRowSkeleton key={i} />
         ))}
       </TableBody>
     </Table>
+  )
+}
+
+export const TableRowSkeleton = () => {
+  return (
+    <TableRow>
+      <TableCell>
+        <Skeleton className="h-4 w-24" />
+      </TableCell>
+      <TableCell className="w-10">
+        <Skeleton className="h-4 w-10" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="h-4 w-4" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="h-4 w-16" />
+      </TableCell>
+    </TableRow>
   )
 }
